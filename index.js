e120b9585918cba81ee966cab13528b648d9171a
Phong-Kaster <phongkaster@gmail.com>
2021-06-03 16:29:01 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var cover = document.createElement("div");
+
+let css = `
+
+position: fixed;
+
+pointer-events: none;
+
+top: 0;
+
+left: 0;
+
+width: 100vw;
+
+height: 100vh;
+
+background-color: white;
+
+mix-blend-mode: difference;
+
+z-index: 1;
+
+`
+
+cover.setAttribute("style", css);
+
+document.body.appendChild(cover);
